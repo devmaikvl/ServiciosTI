a7b052f99afb13f27a25ad2b26067783c72c8c9d
devmaikvl <devmaikvl@gmail.com>
2025-06-11 09:18:46 -0600

    Add files via upload

---
+++ ServiciosTI/ServiciosTI/src/main/java/com/ServiciosTI/ServiciosTI/ServiciosTiApplication.java
@@ -0,0 +1,13 @@
+package com.ServiciosTI.ServiciosTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosTiApplication.class, args);
+	}
+
+}
---
+++ ServiciosTI/ServiciosTI/src/test/java/com/ServiciosTI/ServiciosTI/ServiciosTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiciosTI.ServiciosTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
